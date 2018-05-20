feb6b720482b938eb2ff363177e96c35c35390d0
minomi <5minhominho@gmail.com>
2018-05-21 03:58:55 +0900

    Model Review를 테스트 하기 위한 준비

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ReviewProvider(models.Model):
+    pass
+
+
+class Book(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
---
+++ books/tests/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+class ReviewsTestCase(TestCase):
+
+    def test_reviews_should_have_title(self):
+        self.fail()
+
+    def test_reviews_should_have_content(self):
+        self.fail()
+
+    def test_reviews_should_have_own_provider(self):
+        self.fail()
+
+    def test_review_title_cannot_exceed_100(self):
+        self.fail()
