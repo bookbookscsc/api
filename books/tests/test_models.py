b36553f09e97f74ad24647a4b35af195e103d927
minomi <5minhominho@gmail.com>
2018-05-22 22:00:24 +0900

    ValidationTestCase 라는 슈퍼 클래스 정의, 중복 되는 코드 정리

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -5,47 +5,54 @@ from ..models import Review, Book
 from mixer.backend.django import mixer
 
 
+class ValidationTestCase(TestCase):
+    def validation_check(self, model):
+        with self.assertRaises(ValidationError):
+            model.full_clean()
+
+
 # Create your tests here.
-class ReviewValidationTestCase(TestCase):
+class ReviewValidationTestCase(ValidationTestCase):
 
     def setUp(self):
         self.review = mixer.blend(Review)
 
     def test_reviews_should_have_title(self):
         self.review.title = ""
-        with self.assertRaises(ValidationError):
-            self.review.full_clean()
+        self.validation_check(self.review)
 
     def test_reviews_should_have_content(self):
         self.review.content = ""
-        with self.assertRaises(ValidationError):
-            self.review.full_clean()
+        self.validation_check(self.review)
 
     def test_review_title_cannot_exceed_100(self):
         self.review.title = "a" * 101
-        with self.assertRaises(ValidationError):
-            self.review.full_clean()
+        self.validation_check(self.review)
 
     def test_range_of_review_star_should_be_from_0_to_5(self):
         self.review.star = 8
-        with self.assertRaises(ValidationError):
-            self.review.full_clean()
+        self.validation_check(self.review)
 
 
-class BookValidationTestCase(TestCase):
+class BookValidationTestCase(ValidationTestCase):
 
     def setUp(self):
         self.book = mixer.blend(Book)
 
     def test_isbn_length_should_be_13(self):
         self.book.isbn = '11'
-        with self.assertRaises(ValidationError):
-            self.book.full_clean()
+        self.validation_check(self.book)
         self.book.isbn = '1' * 14
-        with self.assertRaises(ValidationError):
-            self.book.full_clean()
+        self.validation_check(self.book)
 
     def test_isbn_should_be_expressed_number(self):
         self.book.isbn = 'a' * 13
-        with self.assertRaises(ValidationError):
-            self.book.full_clean()
+        self.validation_check(self.book)
+
+    def test_book_title_cannot_exceed_120(self):
+        self.book.title = 'a' * 121
+        self.validation_check(self.book)
+
+    def test_book_should_have_title(self):
+        self.book.title = ''
+        self.validation_check(self.book)
